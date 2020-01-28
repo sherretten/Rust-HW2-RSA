4740ab0c85b417972f60b3367d68f07993dc3469
Nathaniel <nathaniels2016@gmail.com>
2020-01-27 18:25:17 -0800

    Working better now, have to :wa in vscode on laptop then cargo build

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
 use toy_rsa_lib::*;
-
+use std::convert::{TryFrom, TryInto};
 pub const EXP: u64 = 65_537;
 
 pub fn genkey() -> (u32, u32) {
     let mut p: u32 = 0;
     let mut q: u32 = 0;
-    let mut a: u32 = 0;
+    let mut a: u64 = 0;
 
     loop {
         p = rsa_prime();
         q = rsa_prime();
-        a = lambda(p, q);
-        if EXP < a.try_into().unwrap() && gcd(EXP, a) == 1 {
+        a = lambda(p, q) as u64;
+        if EXP < a && gcd(EXP, a) == 1 {
             break;
         }
     }
-
     (p, q)
 }
 
-pub fn lambda(p: u32, q: u32) {
-    lcm(p - 1, q - 1)
+pub fn lambda(p: u32, q: u32) -> u64 {
+    let mut a: u64 = lcm((p - 1).into(), (q - 1).into());
+    return a;
 }
 
 pub fn encrypt(key: u64, msg: u32) -> u64 {
@@ -28,17 +28,17 @@ pub fn encrypt(key: u64, msg: u32) -> u64 {
 }
 
 pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
-    let d: u64 = modinverse(lambda(key.0, key.0), EXP); // inverse of lambda p,q mod E
+    let d: u64 = modinverse(lambda(key.0, key.0).into(), EXP); // inverse of lambda p,q mod E
 
-    modexp(msg, d, key.0 * key.1)
+    modexp(msg, d, (key.0 * key.1).into()).try_into().unwrap()
 }
 
 fn main() {
-    let p = 0xed23e6cd;
+    // let p: u32 = 0xed23e6cd;
 
-    let q = 0xf050a04d;
+    // let q: u32 = 0xf050a04d;
 
-    let publicKey = genkey();
+    // let publicKey = genkey();
 
-    let Message = 12345;
+    // let Message = 12345;
 }
