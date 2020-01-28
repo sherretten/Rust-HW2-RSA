a6ea28209fe5fb5303bf70191b59dd2b7fa2f97e
Nathaniel <nathaniels2016@gmail.com>
2020-01-27 18:02:33 -0800

    Idk wtf vscode is doing

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,44 @@
 use toy_rsa_lib::*;
-pub const EXP = 65_537;
 
-pub fn genkey() -> (u32, u32){
+pub const EXP: u64 = 65_537;
+
+pub fn genkey() -> (u32, u32) {
+    let mut p: u32 = 0;
+    let mut q: u32 = 0;
+    let mut a: u32 = 0;
+
     loop {
-        let p = rsa_prime();
-        let q = rsa_prime();
-        let a: u32 = lambda(p, q);
-        if EXP < a.try_into().unwrap() && gcd(EXP, a) == 1 { return (p, q); }
+        p = rsa_prime();
+        q = rsa_prime();
+        a = lambda(p, q);
+        if EXP < a.try_into().unwrap() && gcd(EXP, a) == 1 {
+            break;
+        }
     }
-    
+
+    (p, q)
 }
 
-pub fn lambda ( p: u32, q:u32){
-  lcm(p-1, q-1)
+pub fn lambda(p: u32, q: u32) {
+    lcm(p - 1, q - 1)
 }
+
 pub fn encrypt(key: u64, msg: u32) -> u64 {
-  modexp(msg, EXP, key)
+    modexp(msg as u64, EXP, key)
 }
 
 pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
-  let d = 1; // inverse of lambda p,q mod E
-  modexp(msg, d, (key.0)* (key.1))
+    let d: u64 = modinverse(lambda(key.0, key.0), EXP); // inverse of lambda p,q mod E
+
+    modexp(msg, d, key.0 * key.1)
 }
 
 fn main() {
-  let p = 0xed23e6cd; 
-  let q = 0xf050a04d;
-  let publicKey = genkey();
-  let Message = 12345;
+    let p = 0xed23e6cd;
 
+    let q = 0xf050a04d;
+
+    let publicKey = genkey();
+
+    let Message = 12345;
 }
