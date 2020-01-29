9546d4885b0fe3edb9cc00deb8bd702851d2e4ee
Nathaniel Sherrett <41343245+sherretten@users.noreply.github.com>
2020-01-28 22:06:58 -0800

    done?

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,24 @@
 use toy_rsa_lib::*;
-use std::convert::{TryFrom, TryInto};
+use std::convert::{TryInto};
 pub const EXP: u64 = 65_537;
 
 pub fn genkey() -> (u32, u32) {
-    let mut p: u32 = 0;
-    let mut q: u32 = 0;
-    let mut a: u64 = 0;
+    // let mut p: u32 = 0;
+    // let mut q: u32 = 0;
+    // let mut a: u64 = 0;
 
     loop {
-        p = rsa_prime();
-        q = rsa_prime();
-        a = lambda(p, q) as u64;
+        let p: u32 = rsa_prime();
+        let q: u32 = rsa_prime();
+        let a: u64 = lambda(p, q);
         if EXP < a && gcd(EXP, a) == 1 {
-            break;
+          return (p,q);
         }
     }
-    (p, q)
 }
 
 pub fn lambda(p: u32, q: u32) -> u64 {
-    let mut a: u64 = lcm((p - 1).into(), (q - 1).into());
-    return a;
+    lcm((p - 1).into(), (q - 1).into())
 }
 
 pub fn encrypt(key: u64, msg: u32) -> u64 {
@@ -28,17 +26,9 @@ pub fn encrypt(key: u64, msg: u32) -> u64 {
 }
 
 pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
-    let d: u64 = modinverse(lambda(key.0, key.0).into(), EXP); // inverse of lambda p,q mod E
-
-    modexp(msg, d, (key.0 * key.1).into()).try_into().unwrap()
+    let d: u64 = modinverse(EXP, lambda(key.0, key.1).into()); // inverse of lambda p,q mod E
+    let n: u64 = (key.0 as u64) * (key.1 as u64);
+    modexp(msg, d, n).try_into().expect("Overflow")
 }
 
-fn main() {
-    // let p: u32 = 0xed23e6cd;
 
-    // let q: u32 = 0xf050a04d;
-
-    // let publicKey = genkey();
-
-    // let Message = 12345;
-}
